3ece96e648b4a027718b4710f5e9091064cd639d
LenoXian <291167494@qq.com>
2017-08-21 13:38:37 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+PROJECT_NAME=moyu
+PROJECT_HOME=/home/admin/app/
+JAR_DIR=${PROJECT_NAME}-core/target
+JAR_NAME=${PROJECT_NAME}.jar
+cd ${PROJECT_HOME}
+ls
+mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
+cd ${JAR_DIR}
+MOYU_PID=`ps -ef | grep "moyu" | grep -v grep | awk '{print $2}'`
+echo "Moyu PID = $MOYU_PID"
+#kill -9 MOYU_PID
+java -jar ${JAR_NAME} --spring.profiles.active=dev
